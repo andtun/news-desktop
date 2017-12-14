02f9f977a8a7a54f053d17c4ff87d4db24268e2b
Ilya <KIM9999999@mail.ru>
2017-12-14 23:36:23 +0300

    Update Post.java

--- Post.java
+++ Post.java
@@ -1,22 +1,37 @@
 import java.util.Date;
 
-class Post {
-    protected String source;
+public class Post {
+    protected String sourse;
     protected String pid;
     protected Date date;
     protected String link;
     protected String description;
     protected String imageUrl;
-    protected Integer rate;
+    protected Integer likes;
+    protected Integer reposts;
 
+    @Override
+    public String toString() {
+        return "Post{" +
+                "sourse:'" + sourse + '\'' +
+                ", pid:'" + pid + '\'' +
+                ", date:" + date +
+                ", link:'" + link + '\'' +
+                ", description:'" + description + '\'' +
+                ", imageUrl:'" + imageUrl + '\'' +
+                ", likes:" + likes +
+                ", reposts:" + reposts +
+                '}';
+    }
 
-    Post (String source, String pid, Date date, String link, String description, String imageUrl, Integer rate){
-        this.source = source;
-        this.pid = pid;
+    Post(String sourse, String pid, Date date,String description, String link, String imageUrl, Integer likes, Integer reposts){
         this.date = date;
-        this.link = link;
         this.description = description;
         this.imageUrl = imageUrl;
-        this.rate = rate;
+        this.link = link;
+        this.pid = pid;
+        this.likes = likes;
+        this.reposts = reposts;
+        this.sourse = sourse;
     }
 }
